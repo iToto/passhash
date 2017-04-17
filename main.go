11373b83e0c500395af29e9b7da3ab0be81291fb
Salvatore D'Agostino <toto19@gmail.com>
2017-04-17 15:28:57 -0400

    initial import of passhash program

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"os"
+)
+
+func main() {
+	for {
+		fmt.Print("Enter string to encrypt: ")
+		var input string
+		fmt.Scanln(&input)
+		fmt.Print()
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input), bcrypt.DefaultCost)
+		if err != nil {
+			fmt.Errorf("Error while hashing: %s", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Hashed: %q", hashedPassword)
+		fmt.Println("")
+	}
+}
